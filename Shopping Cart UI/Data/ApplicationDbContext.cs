bae209ff28a5a4ea332a1fe2954ae9ae7a9b37db
Manish.Gupta <manish173481@gmail.com>
2024-04-10 16:38:49 +0530

    Add project files.

---
+++ Shopping Cart UI/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shopping_Cart_UI.Models;
+
+namespace Shopping_Cart_UI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }        
+        public DbSet<Book> Books { get; set; }  
+        public DbSet<ShoppingCart> ShoppingCarts { get; set;}
+        public DbSet<CartDetail> CartDetails { get; set;}
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderDetail> ordersDetails { get; set; }
+        public DbSet<OrderStatus> ordersStatus { get; set; }
+
+
+    }
+}
---
+++ Shopping Cart UI/Models/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shopping_Cart_UI.Models
+{
+    [Table("ShoppingCart")]
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public ICollection<CartDetail> CartDetails { get; set; }
+    }
+}
